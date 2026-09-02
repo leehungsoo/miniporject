5a8c1f2c611a6f6764a25af8f7c3908e0b0b8e05
kimprogress <kili62@naver.com>
2026-09-02 12:22:38 +0900

    RestayController 수정

--- src/main/java/bitc/aws402/miniproject/controller/RestayController.java
+++ src/main/java/bitc/aws402/miniproject/controller/RestayController.java
@@ -25,27 +25,6 @@ public class RestayController {
         model.addAttribute("roomList", roomList);
         return "room/RoomList";
     }
-    // roomA 진입
-    @GetMapping("/RoomList/roomA")
-    public String roomA(@RequestParam("id") int roomIdx, Model model) {
-        RestayDTO room = detailService.selectRoomDetail(roomIdx);
-        model.addAttribute("room", room);
-        return "room/roomA";
-    }
-    // roomB 진입
-    @GetMapping("/RoomList/roomB")
-    public String roomB(@RequestParam("id") int roomIdx, Model model) {
-        RestayDTO room = detailService.selectRoomDetail(roomIdx);
-        model.addAttribute("room", room);
-        return "room/roomB";
-    }
-    // roomC 진입
-    @GetMapping("/RoomList/roomC")
-    public String roomC(@RequestParam("id") int roomIdx, Model model) {
-        RestayDTO room = detailService.selectRoomDetail(roomIdx);
-        model.addAttribute("room", room);
-        return "room/roomC";
-    }
     // 부대시설 조회
     @GetMapping("/Facilities")
     public String Facilities() {
@@ -55,5 +34,16 @@ public class RestayController {
     @GetMapping("/RoomIntro")
     public String RoomIntro() {
         return "room/RoomIntro";
+    }
+
+    @GetMapping("/RoomList/RoomN")
+    public String getRoomDetail(@RequestParam("id") int roomIdx, Model model) {
+        RestayDTO room = detailService.selectRoomDetail(roomIdx);
+
+        List<RestayDTO> resources = detailService.selectResourceList(roomIdx);
+
+        model.addAttribute("room", room);
+        model.addAttribute("resources", resources);
+        return "room/RoomN";
     }
 }
